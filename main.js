2275329ea82e81a7fa87dca2363207b5173389ba
lion <s1098539@student.hsleiden.nl>
2018-05-14 22:50:19 +0200

    move animation

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-window.onload = function () {
+window.onload = function() {
     var box1 = document.getElementById('box1');
     var box2 = document.getElementById('box2');
     var box3 = document.getElementById('box3');
     var box4 = document.getElementById('box4');
     var camera = document.getElementById('camera');
     var cameraAnimation = document.getElementById('cameraAnimation');
-    box1.addEventListener('mouseenter', function () {
-            camera.setAttribute('position', '-7 0 0');
-          }
-    );
-    box2.addEventListener('mouseenter', function () {
-            camera.setAttribute('position', '-2 0 0');
-          }
-    );
-    box3.addEventListener('mouseenter', function () {
-            camera.setAttribute('position', '6 0 0');
-          }
-    );
-    box4.addEventListener('mouseenter', function () {
-            camera.setAttribute('position', '8 0 0');
-          }
-    );
-  };
+
+    AFRAME.registerComponent('move-on-mouseenter', {
+        schema: {
+            to: {
+                default: '0 0 0' 
+            }
+        },
+        init: function() {
+            var data = this.data;
+            this.el.addEventListener('mouseenter', function() {
+                let anim = document.createElement('a-animation');
+                anim.setAttribute('attribute', 'position');
+                anim.setAttribute('dur', '2000');
+                anim.setAttribute('easing', 'linear');
+                anim.setAttribute('to', data.to);
+                camera.append(anim);
+            });
+        }
+    });
+};
