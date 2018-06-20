769b5005121779b8e454e0389e7ca8205481b8f4
lion <s1098539@student.hsleiden.nl>
2018-06-20 15:50:16 +0200

    pls work

--- main.js
+++ main.js
@@ -328,8 +328,8 @@ window.onload = function () {
             let x = camera.getAttribute('position').x - .05;
             if (x > -10) {
                 let z = camera.getAttribute('position').z;
-                camera.setAttribute('position', x + " -.5 " + z);
-                handBox.setAttribute('position', x + " -.5 " + z);
+                camera.setAttribute('position', x + " -1 " + z);
+                handBox.setAttribute('position', x + " -1 " + z);
             }
         }
     };
@@ -339,8 +339,8 @@ window.onload = function () {
             let x = camera.getAttribute('position').x + .05;
             if (x < 6.5) {
                 let z = camera.getAttribute('position').z;
-                camera.setAttribute('position', x + " -.5 " + z);
-                handBox.setAttribute('position', x + " -.5 " + z);
+                camera.setAttribute('position', x + " -1 " + z);
+                handBox.setAttribute('position', x + " -1 " + z);
             }
         }
     };
