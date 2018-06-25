9d70ea0b3ebb4165a98a65491b8f1d249dc0ec80
Jelle_M <jelle.metzlar@outlook.com>
2018-06-25 11:50:58 +0200

    Forgotten push, less imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,6 @@ import {Vec3} from "./Vec3";
 import {TakeAndPlaceObjective} from "./objectives/generic/TakeAndPlaceObjective";
 import {PickupObjective} from "./objectives/generic/PickupObjective";
 import {CloseFridgeObjective} from "./objectives/generic/CloseFridgeObjective";
-import {Objective01_A} from "./objectives/Objective01_A";
-import {Objective01_B} from "./objectives/Objective01_B";
-import {Objective01_C} from "./objectives/Objective01_C";
-import {Objective03} from "./objectives/Objective03";
-import {Objective04_B} from "./objectives/Objective04_B";
-import {Objective04_A} from "./objectives/Objective04_A";
-import {Objective04} from "./objectives/Objective04";
-import {Objective05_A} from "./objectives/Objective05_A";
-import {Objective05_B} from "./objectives/Objective05_B";
 
 export class Application {
     static instance: Application;
@@ -42,9 +33,9 @@ export class Application {
             new TakeAndPlaceObjective('onion', new Vec3(-5.2, -2, 4.8), new Vec3(-10, -1.7, 1), new Vec3(0, 90, 0)),
             new CloseFridgeObjective(new Vec3(-3, -1,   8), new Vec3(0, 180, 0)),
             new PickupObjective('knife', new Vec3(-3, -1, 8), new Vec3(0, 180, 0), new Vec3(218, 180, 270)),
-            new Objective01_A(),
-            new Objective01_B(),
-            new Objective01_C(),
+            new objectives.Objective01_A(),
+            new objectives.Objective01_B(),
+            new objectives.Objective01_C(),
             new TakeAndPlaceObjective('bowl', new Vec3(-4, -2.2, -6.25), new Vec3(-5, -.5, 7), new Vec3(0, 180, 0)),
             new objectives.Objective02(),
             new objectives.Objective02_A(),
@@ -53,20 +44,19 @@ export class Application {
             new objectives.Objective02_C(),
             new CloseFridgeObjective(),
             new TakeAndPlaceObjective('spaghet-pan', new Vec3(-0.55, -3, -5.72)),
-            new Objective03(),
+            new objectives.Objective03(),
             new TakeAndPlaceObjective('spaghet-pan', new Vec3(-6.5, -2.2, -6)),
             new TimeoutObjective("Don't forget to steer\nthe grounded beef.", 5000),
-            new Objective04(),
+            new objectives.Objective04(),
             new OpenFridgeObjective(),
-            new Objective04_A(),
+            new objectives.Objective04_A(),
             new CloseFridgeObjective(),
-            new Objective04_B(),
+            new objectives.Objective04_B(),
             new TimeoutObjective('Put the sauce on a low fire', 5000),
             new TimeoutObjective('wait for about\n15 minutes', 5000),
             new TakeAndPlaceObjective('strainer', new Vec3(-0.55, -2.5, -4)),
-            new Objective05_A(),
-            new Objective05_B(),
-
+            new objectives.Objective05_A(),
+            new objectives.Objective05_B(),
             new objectives.ObjectiveFinale(),
         ];
 
